71e6b67ff7308dfe13b2ecf48be0b2e35b7c8068
thebeardeddoctor <beardeddoctor29@gmail.com>
2022-09-23 15:23:43 -0500

    added logic to game

--- rps.js
+++ rps.js
@@ -1,47 +1,103 @@
-// Pseudocode
-// 
-// get response from player
-// assign to playerEntry
-// get random response from computer
-// assign to computerEntry
-// give both to function rockPaperScissors
-// compare entries from computer and player
-// if player wins, update their score count and give response "you win"
-// if computer wins, increment computers score count and tell user "you lose"
-// stop game when the best of 5 is figured out 
-const 
+
+const rock = document.getElementById('🪨');
+const paper = document.getElementById('🧻');
+const scissors = document.getElementById('✂️');
+const userGuess= document.getElementById('userGuess')
+const compGuess=document.getElementById('compGuess')
+const round = document.getElementById('round')
+const compScore= document.getElementById('computerscore')
+const playerScore=document.getElementById('playerscore')
+const results = document.querySelector('.endgame')
+const resetbtn=document.querySelector('#resetbtn')
+
 let userScore=0;
 let computerScore=0;
-  for (let i=0; i<5; i++){
-    let gameWeapons = ["rock", "paper", "scissors"];
-    let playerPrompt = prompt("Rock, Paper or Scissors?");
-    let computerEntry= gameWeapons[Math.floor(Math.random()*gameWeapons.length)];
-    let playerEntry= playerPrompt.toLowerCase();
-    
-    if (gameWeapons.includes(playerEntry)===false){
-      alert("stop throwing gang signs!");
-      i--;
-    } else if (computerEntry===playerEntry) {
-      alert("We have a draw! Please guess again!");
-      i--;
-    } else if (computerEntry==="rock" && playerEntry==="paper"){
-      alert("You win! Paper COVERS rock!");
+let computerEntry='';
+let playerEntry='';
+let gameWeapons = ["🪨", "🧻", "✂️"];
+let outcome='';
+
+
+
+rock.addEventListener('click', ()=>{
+  userGuess.textContent=`Player:\n ${rock.id}`;
+  playerEntry='🪨';
+  computerguess();
+})
+
+paper.addEventListener('click', ()=>{
+  userGuess.textContent=`Player:\n ${paper.id}`;
+  playerEntry="🧻"
+  computerguess();
+})
+scissors.addEventListener('click', ()=>{
+  userGuess.textContent=`Player:\n ${scissors.id}`;
+  playerEntry='✂️'
+  computerguess();
+})
+
+
+
+function computerguess(){ 
+  computerEntry= gameWeapons[Math.floor(Math.random()*gameWeapons.length)];
+  compGuess.textContent=`Computer:\n ${computerEntry}`
+  checkWinner();
+}
+
+  function checkWinner(){ 
+    console.log(playerEntry)
+    console.log(computerEntry) 
+    if (computerEntry===playerEntry) {
+      outcome="We have a draw! Please guess again!";
+    } else if (computerEntry==="🪨" && playerEntry==="🧻"){
+      outcome="You win! 🧻 COVERS 🪨!";
       userScore++;
-    } else if(computerEntry==="paper" && playerEntry==="scissors"){
-      alert("You win! scissors CUTS paper!");
+    } else if(computerEntry==="🧻" && playerEntry==="✂️"){
+      outcome="You win! ✂️ CUTS 🧻!";
       userScore++;
-    } else if (computerEntry==="scissors" && playerEntry==="rock"){
-      alert("You win! rock SMASHES scissors!");
+    } else if (computerEntry==="✂️" && playerEntry==="🪨"){
+      outcome="You win! 🪨 SMASHES ✂️!";
       userScore++;
-    } else if (playerEntry==="rock" && computerEntry==="paper"){
-      alert("You lose! Paper COVERS rock!");
+    } else if (playerEntry==="🪨" && computerEntry==="🧻"){
+      outcome="You lose! 🧻 COVERS 🪨!";
       computerScore++;
-    } else if(playerEntry==="paper" && computerEntry==="scissors"){
-      alert("You lose! scissors CUTS paper!");
+    } else if(playerEntry==="🧻" && computerEntry==="✂️"){
+      outcome="You lose! ✂️ CUTS 🧻!";
       computerScore++;
-    } else if (playerEntry==="scissors" && computerEntry==="rock"){
-      alert("You lose! rock SMASHES scissors!");
+    } else if (playerEntry==="✂️" && computerEntry==="🪨"){
+      outcome="You lose! 🪨 SMASHES ✂️!";
       computerScore++;
   }
+    round.textContent=outcome;
+    compScore.textContent=`${computerScore}`;
+    playerScore.textContent=`${userScore}`;
+    if (userScore===10||computerScore===10){
+      endGame();
+    }
+    }
+  
+  function resetGame(){
+    playerScore.textContent=0;
+    compScore.textContent=0;
+    userGuess.textContent='Player:';
+    compGuess.textContent='Computer:';
+    round.textContent=''
+    userScore=0;
+    computerScore=0;
+    computerEntry='';
+    playerEntry='';
+    outcome='';
+  }
+function endGame(){
+  if(userScore==10){
+     round.textContent="Congrats! You've Won!" 
+     resetGame()
+  } else if(computerScore==10){
+    round.textContent="Sorry! You Lose!"
+    resetGame()
+  }
 }
-  alert(`your scored ${userScore} points and the computer scored ${computerScore} `);
+
+resetbtn.addEventListener('click',
+  resetGame
+ )
